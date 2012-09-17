75eb63538aa527e7af1e666e153fbf8b7b831a5e
Robbie Clutton <robert.clutton@gmail.com>
2012-09-16 21:48:15 -0400

    init

---
+++ lib/tracker-git.rb
@@ -0,0 +1,7 @@
+require "tracker-git/version"
+
+module Tracker
+  module Git
+    # Your code goes here...
+  end
+end
---
+++ lib/tracker-git/version.rb
@@ -0,0 +1,5 @@
+module Tracker
+  module Git
+    VERSION = "0.0.1"
+  end
+end
