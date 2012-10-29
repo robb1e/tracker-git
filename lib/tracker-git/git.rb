1421ed312d2bd37e539839191e61542d5e9adab3
John Barker <jebarker@gmail.com>
2012-10-29 15:07:34 -0400

    Put branch before grep in Git#contains?

--- lib/tracker-git/git.rb
+++ lib/tracker-git/git.rb
@@ -2,7 +2,7 @@ module Tracker
   class Git
     def contains?(message, options = {})
       branch = options.fetch(:branch, "master")
-      result = `git log --grep='#{message}' -- #{branch}`
+      result = `git log #{branch} --grep='#{message}'`
       result.length > 0
     end
   end
